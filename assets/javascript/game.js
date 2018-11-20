0ee430ff65993a5934a376bd91c77e4dc1f0e62b
Minal Karani <karani.minal@gmail.com>
2018-11-20 18:22:45 -0500

    Hangman

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,8 +1,10 @@
 var wordList= ["chrome", "firefox", "javascript", "jquery", "twitter", "github", "wordpress", "opera", "sass", "layout", "standards", "semantic", "designer", "developer", "module", "component", "website", "creative", "banner", "browser", "screen", "mobile", "footer", "header", "typography", "responsive", "programmer", "css", "border", "compass", "grunt", "pixel", "document", "object", "ruby", "bootstrap", "python", "php", "pattern", "ajax", "node", "element", "android", "application", "adobe", "apple", "google", "microsoft", "bookmark", "internet", "icon", "svg", "background", "property", "syntax", "flash", "html", "font", "blog", "network", "server", "content", "database", "socket", "function", "variable", "link", "apache", "query", "proxy", "backbone", "angular", "email", "underscore", "cloud"];
 var letters=["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
 var userguess;
+var winflag=true,lossflag=true;
 
 
+//Game object
 game = {
    
     word:[],
@@ -30,8 +32,8 @@ game = {
 };
 
 function wordGuess(userg){
-    
-    while ((game.remainingLetters>0)&&(game.totalnoofGuess<20))
+
+    while ((game.remainingLetters>0)&&(game.totalnoofGuess<15))
     {
     
     
@@ -48,27 +50,49 @@ function wordGuess(userg){
     }
     
     break;
+    winflag=true;
+    lossflag=true;
     }
     userop();
+
+    //User guessed the correct word
     if(game.remainingLetters===0)
-    {
+    {   
+        document.getElementById("wintext").style="display:block";
         userop();
         var html =
           "<p>Good job! The answer was :  " + game.word + "</p>";
           document.querySelector("#wintext").innerHTML = html;
-        game.wins++;
     
         //Play audio after correct guess
+        if(winflag===true)
+        {
         var audio = new Audio('assets/Audio/winaudio.wav');
         audio.play();
-    
+        game.wins++;
+        winflag=false;
         }
-    else if (game.totalnoofGuess>=20){
+        document.getElementById("wordgame").style="display:none";
+        }
+
+    // User unable to guess the word
+    else if (game.totalnoofGuess>=15){
+        document.getElementById("wintext").style="display:block";
         var html =
           "<p>Attemps exceeded the answer was :  " + game.word + "</p>";
           document.querySelector("#wintext").innerHTML = html;
+
+        //Play audio after wrong guess
+        if(lossflag===true)
+        {
+        var audio = new Audio('assets/Audio/looseaudio.wav');
+        audio.play();
         game.losses++;
+        lossflag=false;
+        }
         userop();
+        document.getElementById("wordgame").style="display:none"
+
     }
      
 }
@@ -99,10 +123,14 @@ document.onkeyup = function(event) {
         }
     wordGuess(userguess);
     }
+
 }
 
+//continue playing game
 function playagain()
 {
+    winflag=true;
+    lossflag=true;
     while(game.answerArray.length>0)
     {
         game.answerArray.pop();
@@ -114,14 +142,21 @@ function playagain()
     game.pickword();
     game.setansarray();   
     userop();
+    document.getElementById("wintext").style="display:none";
+    document.getElementById("wordgame").style="display:block";
 }
+
+//Start the game
 function startplay()
 {
     var html =
           "<p>Enter a letter !" +"</p>" +
           "<p>Hint: words are web related." + "</p>";
+          
           document.querySelector("#user-text").innerHTML = html;
           game.pickword();
           game.setansarray();   
           userop();
-}
+          document.getElementById("btnstart").style="display:none";
+          document.getElementById("btn").style="visibility:visible";
+} 
